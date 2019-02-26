bf392681a5c1de397d37457dbd8945b2b6b064d1
priyankathorat31 <priyanka55411@gmail.com>
2019-02-26 15:58:39 +0530

    End of Section14

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -25,7 +25,7 @@ getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>>
   params = params.append('pageSize', itemsPerPage);
 }
 
-if(userParams != null) {
+if (userParams != null) {
   params = params.append('minAge', userParams.minAge);
   params = params.append('maxAge', userParams.maxAge);
   params = params.append('gender', userParams.gender);
